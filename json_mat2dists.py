60bd816234ac7654187289b08b50731a12ef2ac3
Jody Phelan <jody.phelan@lshtm.ac.uk>
2017-06-19 10:53:48 +0100

    matrix distance format

--- json_mat2dists.py
+++ json_mat2dists.py
@@ -11,8 +11,10 @@ for chrom,pos in data["loci"]:
 		if data["vars"][chrom][str(pos)][s]!="-" and data["vars"][chrom][str(pos)][s]!="N":
 			set_variants[s].add((chrom,pos))
 
-	
+print "\t%s" % ("\t".join(data["samples"]))	
 for s1 in data["samples"]:
+	dists = []
 	for s2 in data["samples"]:
 		dist = len(set_variants[s1]-set_variants[s2]) + len(set_variants[s2]-set_variants[s1])
-		print "%s\t%s\t%s" % (s1,s2,dist)
+		dists.append(dist)
+	print "%s\t%s" % (s1,"\t".join(dists))
